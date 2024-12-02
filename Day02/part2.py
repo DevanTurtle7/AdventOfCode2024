9c284e17029249f7eeafa05e19688dfdba70a1d4
Devan Kavalchek <devankav7@gmail.com>
2024-12-02 15:50:17 -0500

    Brute force

--- Day02/part2.py
+++ Day02/part2.py
@@ -21,6 +21,24 @@ def is_valid(delta):
     return delta <= max_delta and delta >= min_delta
 
 
+def is_safe(levels, slope):
+    min_delta = min(MIN_ABS_DELTA * slope, MAX_ABS_DELTA * slope)
+    max_delta = max(MIN_ABS_DELTA * slope, MAX_ABS_DELTA * slope)
+    safe = True
+    i = 0
+
+    while i < len(levels) - 1:
+        delta = levels[i + 1] - levels[i]
+
+        if delta > max_delta or delta < min_delta:
+            safe = False
+            break
+
+        i += 1
+
+    return safe
+
+
 def main():
     safe_count = 0
 
@@ -47,67 +65,17 @@ def main():
             if len(levels) - len(set(levels)) > 1:
                 continue
 
-            sorted = levels.copy()
-            sorted.sort(reverse=general_slope == -1)
-            skipped = set()
-            duplicates = set()
+            safe = False 
 
-            level_i = 0
-            sorted_i = 0
-            diff = 0
-            skipped_index = None
+            if is_safe(levels, general_slope):
+                safe = True
+            else:
+                for i in range(0, len(levels)):
+                    sliced = levels[0:i] + levels[i+1:len(levels)]
+                    if is_safe(sliced, general_slope):
+                        safe = True
+                        break
 
-            while level_i < len(levels) and sorted_i < len(sorted) and diff <= 1:
-                current = levels[level_i]
-                expected = sorted[sorted_i]
-
-                if current == expected and not (level_i > 0 and current == levels[level_i - 1]):
-                    level_i += 1
-
-                    if sorted_i < len(sorted) - 1:
-                        sorted_i += 1
-                else:
-                    if sorted_i > 0 and expected == sorted[sorted_i - 1]:
-                        duplicates.add(expected)
-
-                        if sorted_i < len(sorted) - 1:
-                            sorted_i += 1
-                            continue
-
-                    if expected not in skipped:
-                        skipped_index = level_i
-                        level_i += 1
-                        diff += 1
-                    else:
-                        if sorted_i < len(sorted) - 1:
-                            sorted_i += 1
-                        else:
-                            level_i += 1
-
-                        if skipped_index == None:
-                            skipped_index = level_i
-
-                    skipped.add(current)
-
-            if diff > 1:
-                continue
-            
-            if skipped_index != None:
-                del levels[skipped_index]
-
-            safe = True
-
-            for i in range(0, len(levels) - 1):
-                delta = levels[i + 1] - levels[i]
-
-                if not is_valid(delta) or get_slope(delta) != general_slope:
-                    if diff == 0 and (i == 0 or i == len(levels) - 1):
-                        diff += 1
-                        continue
-
-                    safe = False
-                    break
-            
             if safe:
                 safe_count += 1
 
