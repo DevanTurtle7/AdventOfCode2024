a6b092225d0fb0b3ab739d50be14aaf70601782b
Devan Kavalchek <devankav7@gmail.com>
2024-12-03 13:01:43 -0500

    Finished day 2

---
+++ Day03/part1.py
@@ -0,0 +1,67 @@
+EXPECTED_PREFIX = "mul("
+EXPECTED_SUFFIX = ")"
+
+# Don't want to use regex and this solution is O(n) so it's probably faster anyways
+
+def main():
+    with open('./input.txt') as file:
+        total = 0
+
+        for line in file:
+            line = line.strip()
+
+            index = 0
+
+            before_comma = True 
+            num1_str = None
+            num2_str = None
+            reset = False
+
+            for char in line:
+                if reset == True:
+                    reset = False
+                    index = 0
+                    before_comma = True
+                    num1_str = None
+                    num2_str = None
+
+                if index >= len(EXPECTED_PREFIX):
+                    if char.isdigit():
+                        if before_comma:
+                            if num1_str == None:
+                                num1_str = ""
+                            elif len(num1_str) >= 3:
+                                reset = True
+                                continue
+
+                            num1_str += char
+                        else:
+                            if num2_str == None:
+                                num2_str = ""
+                            elif len(num2_str) >= 3:
+                                reset = True
+                                continue
+
+                            num2_str += char
+                    elif char == "," and num1_str != None:
+                        before_comma = False
+                    elif char == EXPECTED_SUFFIX and num1_str != None and num2_str != None:
+                        total += int(num1_str) * int(num2_str)
+                        reset = True
+                        continue
+                    else:
+                        reset = True
+                        continue
+                else:
+                    if char == EXPECTED_PREFIX[index]:
+                        index += 1
+                    else:
+                        reset = True
+                        continue
+
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ Day03/part2.py
@@ -0,0 +1,94 @@
+EXPECTED_PREFIX = "mul("
+EXPECTED_SUFFIX = ")"
+ENABLE_STR = "do()"
+DISABLE_STR = "don't()"
+
+# Don't want to use regex and this solution is O(n) so it's probably faster anyways
+
+def main():
+    with open('./input.txt') as file:
+        total = 0
+
+        index = 0
+        enable_index = 0
+        disable_index = 0
+
+        before_comma = True 
+        num1_str = None
+        num2_str = None
+        reset = False
+
+        enabled = True
+
+        for line in file:
+            line = line.strip()
+
+            for char in line:
+                if reset == True:
+                    reset = False
+                    index = 0
+                    before_comma = True
+                    num1_str = None
+                    num2_str = None
+
+                if enable_index >= len(ENABLE_STR):
+                    enabled = True
+                    enable_index = 0
+                elif char == ENABLE_STR[enable_index]:
+                    enable_index += 1
+                else:
+                    enable_index = 0
+
+                if not enabled:
+                    continue
+
+                if disable_index >= len(DISABLE_STR):
+                    enabled = False
+                    disable_index = 0
+                    reset = True
+                    continue
+                elif char == DISABLE_STR[disable_index]:
+                    disable_index += 1
+                else:
+                    disable_index = 0
+
+                if index >= len(EXPECTED_PREFIX):
+                    if char.isdigit():
+                        if before_comma:
+                            if num1_str == None:
+                                num1_str = ""
+                            elif len(num1_str) >= 3:
+                                reset = True
+                                continue
+
+                            num1_str += char
+                        else:
+                            if num2_str == None:
+                                num2_str = ""
+                            elif len(num2_str) >= 3:
+                                reset = True
+                                continue
+
+                            num2_str += char
+                    elif char == "," and num1_str != None:
+                        before_comma = False
+                    elif char == EXPECTED_SUFFIX and num1_str != None and num2_str != None:
+                        total += int(num1_str) * int(num2_str)
+                        reset = True
+                        continue
+                    else:
+                        reset = True
+                        continue
+                else:
+                    if char == EXPECTED_PREFIX[index]:
+                        index += 1
+                    else:
+                        reset = True
+                        continue
+
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
+
