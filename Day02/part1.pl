74458c7bb296b8b0aad4cf665b4057ad12cdf733
Devan Kavalchek <devankav7@gmail.com>
2024-12-05 14:55:14 -0500

    Rewrote day 2 in perl

---
+++ Day02/part1.pl
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+use List::Util qw(all min max sum);
+
+my $count = 0;
+
+sub is_safe {
+    my @levels = @_;
+
+    my @slopes = map {
+        my $delta = $levels[$_ + 1] - $levels[$_];
+        $delta == 0 ? 0 : $delta / abs($delta);
+    } (0..$#levels - 1);
+
+    my $slope = sum(@slopes) > 0 ? 1 : -1;
+    my @limits = (1 * $slope, 3 * $slope);
+    my $min_slope = min(@limits);
+    my $max_slope = max(@limits);
+
+    return all {
+        my $delta = $levels[$_ + 1] - $levels[$_];
+        $delta <= $max_slope && $delta >= $min_slope;
+    } (0..$#levels - 1);
+}
+
+open (my $file, "<", "input.txt") or die $!;
+
+while (my $line = <$file>) {
+    my @levels = split(" ", $line);
+
+    if (is_safe(@levels)) {
+        $count++;
+    }
+}
+
+close ($file);
+
+print("$count\n");
