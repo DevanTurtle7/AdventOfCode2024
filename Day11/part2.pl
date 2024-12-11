327f44e14c0287300e0fd649c4df5fcfcc34ce05
Devan Kavalchek <devankav7@gmail.com>
2024-12-11 14:52:24 -0500

    Small refactor

--- Day11/part2.pl
+++ Day11/part2.pl
@@ -3,6 +3,7 @@ use warnings;
 
 my %stone_counts;
 my @stones;
+my $num_blinks = 75;
 
 open (my $file, "<", "input.txt") or die $!;
 
@@ -12,48 +13,36 @@ while (my $line = <$file>) {
 
     foreach my $token (@tokens) {
         push(@stones, $token);
-
         $stone_counts{$token} += 1;
     }
 }
 
 close ($file);
 
-my $total = 0;
-my $num_blinks = 75;
-
 for (1..$num_blinks) {
     my %new_counts;
 
-    foreach my $stone (keys %stone_counts) {
-        $stone += 0; # Force numeric context
-        my $num_stones = $stone_counts{$stone} + 0;
-
+    while (my ($stone, $count) = each %stone_counts) {
         my $num_digits = length($stone);
 
         if ($stone == 0) {
-            $new_counts{1} += $num_stones;
+            $new_counts{1} += $count;
         } elsif ($num_digits % 2 == 0) {
             my $half_length = $num_digits / 2;
             my $first_half = substr($stone, 0, $half_length) + 0;
             my $second_half = substr($stone, $half_length, $num_digits) + 0;
 
-            $new_counts{$first_half} += $num_stones;
-            $new_counts{$second_half} += $num_stones;
+            $new_counts{$first_half} += $count;
+            $new_counts{$second_half} += $count;
         } else {
-            $new_counts{$stone * 2024} += $num_stones;
+            $new_counts{$stone * 2024} += $count;
         }
     }
 
     %stone_counts = %new_counts;
 }
 
-foreach my $stone (keys %stone_counts) {
-    my $count = $stone_counts{$stone};
-
-    if ($count != 0) {
-        $total += $count;
-    }
-}
+my $total = 0;
+$total += $_ for values %stone_counts;
 
 print("Total: $total\n");
