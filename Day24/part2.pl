13b879008802514d4391ed42a371c4c58a530821
Devan Kavalchek <devankav7@gmail.com>
2024-12-24 15:21:10 -0500

    Finished day 24!!!

--- Day24/part2.pl
+++ Day24/part2.pl
@@ -1,8 +1,6 @@
 use strict;
 use warnings;
 
-my %values;
-my @stack;
 my %connections;
 
 open (my $file, "<", "input.txt") or die $!;
@@ -14,103 +12,89 @@ while (my $line = <$file>) {
         $reading_commands = !!1;
         next;
     }
+
     chomp($line);
 
     if ($reading_commands) {
-        push(@stack, $line);
-    } else {
-        my ($wire, $value) = split(": ", $line);
-        $values{$wire} = $value;
+        my ($command_line, $output) = split(" -> ", $line);
+        my ($input1, $command, $input2) = split(" ", $command_line);
+        $connections{$input1}{$command}{$input2} = $output;
+        $connections{$input2}{$command}{$input1} = $output;
     }
-
 }
 
 close ($file);
-my $i = 0;
 
-while ($#stack >= 0) {
-    my $line = $stack[$i];
-    my ($command_line, $output) = split(" -> ", $line);
-    my ($input1, $command, $input2) = split(" ", $command_line);
+my %swaps;
 
-    unless (exists $values{$input1} && exists $values{$input2}) {
-        $i++;
-        next;
-    }
+sub swap {
+    my ($output1, $output2) = @_;
 
-    splice(@stack, $i, 1);
-    my $value1 = !!$values{$input1};
-    my $value2 = !!$values{$input2};
-    my $result;
-
-    if ($command eq "AND") {
-        $result = $value1 && $value2;
-    } elsif ($command eq "OR") {
-        $result = $value1 || $value2;
-    } elsif ($command eq "XOR") {
-        $result = $value1 ^ $value2;
-    }
-
-    $values{$output} = $result + 0;
-    $connections{$input1}{$output} = 1;
-    $connections{$input2}{$output} = 1;
-    $i = 0;
-}
-
-sub get_connections {
-    my ($current, $expected, $connections_ref, $connected_wires) = @_;
-    my %all_connections = %$connections_ref;
-    $connected_wires->{$current} = 1;
-
-    if (substr($current, 0, 1) eq "z") {
-        return;
-    }
-
-    foreach my $connect (keys %{$connections{$current}}) {
-        get_connections($connect, $expected, $connections_ref, $connected_wires);
-    }
-}
-
-my %invalid_wires;
-
-for my $i ("00".."44") {
-    my $expected = "z$i";
-    my %new_connections;
-
-    for my $key (keys %connections) {
-        for my $sub_key (keys %{$connections{$key}}) {
-            $new_connections{$key}{$sub_key} = 1;
-        }
-    }
-
-    if (exists $values{"x$i"}) {
-        my $invalid_connection = !!0;
-        my %connected_wires;
-        get_connections("x$i", $expected, \%new_connections, \%connected_wires);
-
-        foreach my $wire (keys %connected_wires) {
-            print("x$i IS CONNECTED TO $wire\n");
-            if (substr($wire, 0, 1) eq "z") {
-                if ($wire ne $expected) {
-                    #print("Invalid connection from x$i to $wire\n");
-                    $invalid_connection = !!1;
+    foreach my $input1 (keys %connections) {
+        foreach my $command (keys %{$connections{$input1}}) {
+            foreach my $input2 (keys %{$connections{$input1}{$command}}) {
+                my $output = $connections{$input1}{$command}{$input2};
+                if ($output eq $output1 || $output eq $output2) {
+                    if ($output eq $output1) {
+                        $connections{$input1}{$command}{$input2} = $output2;
+                    } else {
+                        $connections{$input1}{$command}{$input2} = $output1;
+                    }
                 }
             }
         }
-
-        unless ($invalid_connection) {
-            print("All valid for x$i\n");
-            next;
-        }
-
-        foreach my $wire (keys %connected_wires) {
-            my %wire_connections;
-            get_connections($wire, $expected, \%new_connections, \%wire_connections);
-            unless (exists $wire_connections{$expected}) {
-                print("Wire $wire is not connected to expected wire $expected\n");
-            } else {
-                print("Wire $wire is fine\n");
-            }
-        }
     }
+
+    $swaps{$output1} = 1;
+    $swaps{$output2} = 1;
 }
+
+my $i = 1;
+
+while ($i <= 44) {
+    my $num = $i."";
+
+    if (length($num) == 1) {
+        $num = "0".$num
+    }
+
+    my $x_start = "x$num";
+    my $y_start = "y$num";
+    my $expected = "z$num";
+
+    my $xor_node = $connections{$x_start}{"XOR"}{$y_start};
+    my $or_node = $connections{$x_start}{"AND"}{$y_start};
+
+    unless (exists $connections{$xor_node}{"XOR"} && exists $connections{$xor_node}{"AND"}) {
+        if (exists $connections{$or_node}{"XOR"} && exists $connections{$or_node}{"AND"}) {
+            swap($xor_node, $or_node);
+        }
+        next;
+    }
+
+    my @xor_connections = keys %{$connections{$xor_node}{"XOR"}};
+    my $xor_other_node = $xor_connections[0];
+    my $xor_output = $connections{$xor_node}{"XOR"}{$xor_other_node};
+
+    if ($xor_output ne $expected) {
+        swap($xor_output, $expected);
+        next;
+    }
+
+    my @and_connections = keys %{$connections{$xor_node}{"AND"}};
+    my $and_other_node = $and_connections[0];
+    my $and_output = $connections{$xor_node}{"AND"}{$and_other_node};
+
+    my @or_connections = keys %{$connections{$or_node}{"OR"}};
+    my $or_other_node = $or_connections[0];
+
+    if ($or_other_node ne $and_output) {
+        swap($or_other_node, $and_output);
+        next;
+    }
+
+    $i++;
+}
+
+my $names = join(",", sort(keys %swaps));
+print("$names\n");
